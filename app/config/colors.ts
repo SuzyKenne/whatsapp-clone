f3bdd271f6c04801fd2d46cd6bb6fa93bc215846
Suzy Kenne <suzyndatewo@gmail.com>
2025-02-18 23:19:40 +0100

    define defaut colors

---
+++ app/config/colors.ts
@@ -0,0 +1,10 @@
+// import { green } from "react-native-reanimated/lib/typescript/Colors";
+
+export default {
+  primary: '#111B21',
+  secondary: '#182229',
+  tertiare: '#374248',
+  white: '#fff',
+  darkGreen: '#09332B',
+  green: '#32BD6A',
+};
