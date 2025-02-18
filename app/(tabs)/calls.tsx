3f30d2690200c2971d97bcb987acd075dcffb252
Suzy Kenne <suzyndatewo@gmail.com>
2025-02-18 23:17:54 +0100

    created calls page

---
+++ app/(tabs)/calls.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Header from '../components/Header';
+
+export default function calls() {
+  return <Header title="Calls" />;
+}
+
+const styles = StyleSheet.create({});
