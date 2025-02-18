d1c4cd9b385be69ae0ff5ef0bcc57c26fb6387c9
Suzy Kenne <suzyndatewo@gmail.com>
2025-02-18 23:18:45 +0100

    whatsapp header component

---
+++ app/components/WhasappHeader.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Header from './Header';
+
+export default function WhasappHeader() {
+  return <Header title="WhatsApp" />;
+}
+
+const styles = StyleSheet.create({});
